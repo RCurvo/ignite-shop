f98c60319e96d1e2aab8576d5b9980ec9f702d2f
RCurvo <renan.curvo@gmail.com>
2022-10-14 15:46:08 -0300

    Implements head in home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,6 +9,7 @@ import 'keen-slider/keen-slider.min.css'
 import { stripe } from '../lib/stripe'
 import { GetStaticProps } from 'next'
 import Stripe from 'stripe'
+import Head from 'next/head'
 
 interface HomeProps {
   products: {
@@ -31,27 +32,30 @@ export default function Home({ products }: HomeProps) {
 
 
   return (
-    <HomeContainer ref={sliderRef} className='keen-slider'>
-      {products.map(product => {
-        return (
-          <Link href={`/product/${product.id}`} key={product.id} prefetch={false}>
-            <Product
-              className='keen-slider__slide'
-            >
-              <Image src={product.imageUrl} alt="" width={520} height={480} />
-              <footer>
-                <strong>
-                  {product.name}
-                </strong>
-                <span>
-                  {product.price}
-                </span>
-              </footer>
-            </Product>
-          </Link>)
-      })}
+    <>
+      <Head><title>Home | Ignite</title></Head>
+      <HomeContainer ref={sliderRef} className='keen-slider'>
+        {products.map(product => {
+          return (
+            <Link href={`/product/${product.id}`} key={product.id} prefetch={false}>
+              <Product
+                className='keen-slider__slide'
+              >
+                <Image src={product.imageUrl} alt="" width={520} height={480} />
+                <footer>
+                  <strong>
+                    {product.name}
+                  </strong>
+                  <span>
+                    {product.price}
+                  </span>
+                </footer>
+              </Product>
+            </Link>)
+        })}
 
-    </HomeContainer>
+      </HomeContainer>
+    </>
   )
 }
 
